00e835cb04cfabf65ba198a6854ed8e630bcac42
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-10-11 10:41:49 +0530

    Add files via upload

---
+++ accum.py
@@ -0,0 +1,10 @@
+"""This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+"""
+def accum(s):
+    return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s))
+
+print(accum("sonu"))
